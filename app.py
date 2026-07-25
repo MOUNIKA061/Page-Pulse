d9f85925920e917e12602ba03ebe5d4efc88bebb
MOUNIKA061 <palavalasamounika06@gmail.com>
2026-07-25 21:20:02 +0530

    modified

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from website_parser import analyze_website
 
@@ -27,4 +28,4 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
